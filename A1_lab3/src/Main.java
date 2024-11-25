2e281608092ddf9f89f8fcd7506153c2e671fcc0
Bogdan640 <bogdanbubu2004@gmail.com>
2024-11-25 15:27:50 +0200

    A3-final

--- A1_lab3/src/Main.java
+++ A1_lab3/src/Main.java
@@ -1,17 +1,14 @@
 import Controller.Ctrl;
-import Controller.ICtrl;
 import Exceptions.RepoException;
 import Model.DataStructures.Classes.MyDictionary;
 import Model.DataStructures.Classes.MyHeap;
 import Model.DataStructures.Classes.MyQueue;
 import Model.DataStructures.Classes.MyStack;
-import Model.Expressions.ArithExp;
-import Model.Expressions.RelationalExp;
-import Model.Expressions.ValueExp;
-import Model.Expressions.VarExp;
+import Model.Expressions.*;
 import Model.PrgState;
 import Model.Statements.*;
 import Model.Types.Classes.IntType;
+import Model.Types.Classes.RefType;
 import Model.Types.Classes.StringType;
 import Model.Values.Classes.IntValue;
 import Model.Values.Classes.StringValue;
@@ -88,17 +85,116 @@ public class Main {
                                         new CompStmt(new WhileStmt(new RelationalExp(new VarExp("a"),"<=", new VarExp("b")),new AssignStmt("a",new ArithExp(new VarExp("a"), "+", new ValueExp(new IntValue(1))))),
                                                 new PrintStmt(new VarExp("a")))))));
 
-        PrgState prg5
+        PrgState prg5 = new PrgState(new MyStack<>(), new MyDictionary<>(), new MyQueue<>(), ex5, new MyDictionary<>(), new MyHeap());
+        IRepo repo5 = new Repo("log5.txt");
+        repo5.addPrgState(prg5);
+        Ctrl ctrl5 = new Ctrl(repo5);
+
+
+        //new statement that allocates, writes and reads from the heap
+        // Example 1: test new heap memory
+        // Ref int v; new(v,20); Ref Ref int a; new(a,v); print(v); print(a);
+        VarDeclStmt varDeclStmt1 = new VarDeclStmt("v", new RefType(new IntType()));
+        HeapAllocationStmt allocStmt1 = new HeapAllocationStmt("v", new ValueExp(new IntValue(20)));
+        VarDeclStmt varDeclStmt2 = new VarDeclStmt("a", new RefType(new RefType(new IntType())));
+        HeapAllocationStmt allocStmt2 = new HeapAllocationStmt("a", new VarExp("v"));
+        PrintStmt printStmt1 = new PrintStmt(new VarExp("v"));
+        PrintStmt printStmt2 = new PrintStmt(new VarExp("a"));
+
+        IStmt ex6 = new CompStmt(varDeclStmt1,
+                new CompStmt(allocStmt1,
+                        new CompStmt(varDeclStmt2,
+                                new CompStmt(allocStmt2,
+                                        new CompStmt(printStmt1, printStmt2)))));
+        
+        PrgState prg6 = new PrgState(new MyStack<IStmt>(), new MyDictionary<>(), new MyQueue<>(), ex6, new MyDictionary<>(), new MyHeap());
+        IRepo repo6 = new Repo("log6.txt");
+        repo6.addPrgState(prg6);
+        Ctrl ctrl6 = new Ctrl(repo6);
+
+        
+        
+        
+        // Example 2: test read heap
+        // Ref int v; new(v,20); Ref Ref int a; new(a,v); print(rH(v)); print(rH(rH(a)) + 5);
+        VarDeclStmt varDeclStmt3 = new VarDeclStmt("v", new RefType(new IntType()));
+        HeapAllocationStmt allocStmt3 = new HeapAllocationStmt("v", new ValueExp(new IntValue(20)));
+        VarDeclStmt varDeclStmt4 = new VarDeclStmt("a", new RefType(new RefType(new IntType())));
+        HeapAllocationStmt allocStmt4 = new HeapAllocationStmt("a", new VarExp("v"));
+        PrintStmt printStmt3 = new PrintStmt(new HeapReadingExp(new VarExp("v")));
+        PrintStmt printStmt4 = new PrintStmt(
+                new ArithExp(new HeapReadingExp(new HeapReadingExp(new VarExp("a"))),"+", new ValueExp(new IntValue(5))));
+
+        IStmt ex7 = new CompStmt(varDeclStmt3,
+                new CompStmt(allocStmt3,
+                        new CompStmt(varDeclStmt4,
+                                new CompStmt(allocStmt4,
+                                        new CompStmt(printStmt3, printStmt4)))));
+
+        PrgState prg7 = new PrgState(new MyStack<IStmt>(), new MyDictionary<>(), new MyQueue<>(), ex7, new MyDictionary<>(), new MyHeap());
+        IRepo repo7 = new Repo("log7.txt");
+        repo7.addPrgState(prg7);
+        Ctrl ctrl7 = new Ctrl(repo7);
+        
+        
+
+        // Example 3: test write heap
+        // Ref int v; new(v,20); print(rH(v)); wH(v,30); print(rH(v) + 5);
+        VarDeclStmt varDeclStmt5 = new VarDeclStmt("v", new RefType(new IntType()));
+        HeapAllocationStmt allocStmt5 = new HeapAllocationStmt("v", new ValueExp(new IntValue(20)));
+        PrintStmt printStmt5 = new PrintStmt(new HeapReadingExp(new VarExp("v")));
+        HeapWritingStmt writeHeap1 = new HeapWritingStmt("v", new ValueExp(new IntValue(30)));
+        PrintStmt printStmt6 = new PrintStmt(
+                new ArithExp(new HeapReadingExp(new VarExp("v")),"+", new ValueExp(new IntValue(5))));
+
+        IStmt ex8 = new CompStmt(varDeclStmt5,
+                new CompStmt(allocStmt5,
+                        new CompStmt(printStmt5,
+                                new CompStmt(writeHeap1, printStmt6))));
+
+        PrgState prg8 = new PrgState(new MyStack<IStmt>(), new MyDictionary<>(), new MyQueue<>(), ex8, new MyDictionary<>(), new MyHeap());
+        IRepo repo8 = new Repo("log8.txt");
+        repo8.addPrgState(prg8);
+        Ctrl ctrl8 = new Ctrl(repo8);
+
+        // Example 4: test garbage collector
+        // Ref int v;new(v,20);Ref Ref int a; new(a,v); new(v,30);print(rH(rH(a)));
+
+        VarDeclStmt varDeclStmt6 = new VarDeclStmt("v", new RefType(new IntType()));
+        HeapAllocationStmt allocStmt6 = new HeapAllocationStmt("v", new ValueExp(new IntValue(20)));
+        VarDeclStmt varDeclStmt7 = new VarDeclStmt("a", new RefType(new RefType(new IntType())));
+        HeapAllocationStmt allocStmt7 = new HeapAllocationStmt("a", new VarExp("v"));
+        HeapAllocationStmt allocStmt8 = new HeapAllocationStmt("v", new ValueExp(new IntValue(30)));
+        PrintStmt printStmt7 = new PrintStmt(new HeapReadingExp(new HeapReadingExp(new VarExp("a"))));
+
+        IStmt ex9 = new CompStmt(varDeclStmt6,
+                new CompStmt(allocStmt6,
+                        new CompStmt(varDeclStmt7,
+                                new CompStmt(allocStmt7,
+                                        new CompStmt(allocStmt8, printStmt7)))));
+
+        PrgState prg9 = new PrgState(new MyStack<IStmt>(), new MyDictionary<>(), new MyQueue<>(), ex9, new MyDictionary<>(), new MyHeap());
+        IRepo repo9 = new Repo("log9.txt");
+        repo9.addPrgState(prg9);
+        Ctrl ctrl9 = new Ctrl(repo9);
+
+
 
 
 
 
         TextMenu menu = new TextMenu();
         menu.addCommand(new ExitCmd("0", "exit"));
-        menu.addCommand(new RunExample("1", ex1.toString(), ctrl1));
-        menu.addCommand(new RunExample("2", ex2.toString(), ctrl2));
-        menu.addCommand(new RunExample("3", ex3.toString(), ctrl3));
-        menu.addCommand(new RunExample("4", ex4.toString(), ctrl4));
+        menu.addCommand(new RunExample("1", ex6.toString(), ctrl1));
+        menu.addCommand(new RunExample("2", ex7.toString(), ctrl2));
+        menu.addCommand(new RunExample("3", ex8.toString(), ctrl3));
+        menu.addCommand(new RunExample("4", ex9.toString(), ctrl4));
+        menu.addCommand(new RunExample("5", ex5.toString(), ctrl5));
+        menu.addCommand(new RunExample("6", ex6.toString(), ctrl6));
+        menu.addCommand(new RunExample("7", ex7.toString(), ctrl7));
+        menu.addCommand(new RunExample("8", ex8.toString(), ctrl8));
+        menu.addCommand(new RunExample("9", ex9.toString(), ctrl9));
+
         menu.show();
 
 
